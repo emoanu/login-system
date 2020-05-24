86db48ee94e418acaa34aba6a8710103d3cd1ddd
Anushree Baghwar <anushreebaghwar03@gmail.com>
2020-05-24 22:13:13 +0530

    fixed session start

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>HomePage</title>
+    <?php
+    include 'bootstrap.php';
+    ?>
+  </head>
+  <body>
+    <div class="container">
+      <br>
+      <br>
+      <?php
+      if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+      }
+      if (isset($_SESSION['username'])) {
+        ?>
+        <h2>Welcome <?php echo $_SESSION['name'] ; ?>!</h2>
+        <h3>username: <?php echo $_SESSION['username'] ; ?>!</h2>
+        <h3>email : <?php echo $_SESSION['email'] ; ?>!</h2>
+        <a class="btn btn-danger" href="logout.php">Logout</a>
+        <?php
+      }
+      else{
+      ?>
+      <a href="login.php" class="btn btn-primary">Login</a>
+      <a href="registration.php" class="btn btn-warning">Register</a>
+    <?php }
+      ?>
+    </div>
+  </body>
+</html>
